d4079e92a42dffe8d835d0c5a35565f974088fbd
ge0nmo <ge0nmo@naver.com>
2024-07-29 23:15:22 +0900

    Thread life cycle

--- src/thread/control/test/JoinTest1Main.java
+++ src/thread/control/test/JoinTest1Main.java
@@ -11,14 +11,14 @@ public class JoinTest1Main
         Thread t2 = new Thread(new MyTask(), "t2");
         Thread t3 = new Thread(new MyTask(), "t3");
 
-        t1.start();
-        t1.join();
+        t1.start(); // 3s
+        t1.join(); // wait
 
-        t2.start();
-        t2.join();
+        t2.start(); // 3s
+        t2.join(); // wait
 
-        t3.start();
-        t3.join();
+        t3.start(); // 3s
+        t3.join(); // wait
         System.out.println("모든 스레드 실행 완료");
     }
 
